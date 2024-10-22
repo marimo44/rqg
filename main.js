6e2cb6cd5614e0931263923020589bbdd3de3bdc
marimo44 <mariaamaba@gmail.com>
2024-10-22 19:27:50 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  let quotes = [];
+
+  $.ajax({
+    url: "./quotes.json",
+    method: "GET",
+    success: function (data) {
+      quotes = data;
+      showQuote();
+    },
+    error: function () {
+      alert("Error fetching data");
+    },
+  });
+
+  const showQuote = () => {
+    const randQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+    $("#text").text(randQuote.quote);
+    $("#author").text("– " + randQuote.author);
+  };
+
+  $("#new-quote").click(function () {
+    showQuote();
+  });
+
+  $("#tweet-quote").click(function () {
+    const quote = $("#text").text();
+    const author = $("#author").text();
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+      quote + "\n" + author
+    )}`;
+    window.open(tweetUrl, "_blank");
+  });
+});
